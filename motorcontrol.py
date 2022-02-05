701292a40212a4f0c30eae8412ee3ac74da23838
trierardi <56281905+trierardi@users.noreply.github.com>
2022-02-05 17:43:10 -0500

    Fix variable names

--- motorcontrol.py
+++ motorcontrol.py
@@ -8,11 +8,11 @@ STEP_PIN = 4
 
 def motor_up():
     motor = RpiMotorLib.A3967EasyNema(DIRECTION_PIN, STEP_PIN, GPIO_PINS)
-    mymotortest.motor_move(.004, 600, False, True, "Full", .05)
+    motor.motor_move(.004, 600, False, True, "Full", .05)
     GPIO.cleanup()
 
 
 def motor_down():
     motor = RpiMotorLib.A3967EasyNema(DIRECTION_PIN, STEP_PIN, GPIO_PINS)
-    mymotortest.motor_move(.004, 600, False, True, "Full", .05)
+    motor.motor_move(.004, 600, False, True, "Full", .05)
     GPIO.cleanup()
